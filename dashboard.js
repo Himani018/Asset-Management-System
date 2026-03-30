a649e41e665ef1413749e995cb3545217e442617
Himani018 <himanibahuguna69@gmail.com>
2026-03-30 15:48:36 +0530

    modify all the secotions

--- dashboard.js
+++ dashboard.js
@@ -47,3 +47,76 @@ function logout() {
     localStorage.removeItem("username");
     alert("Logged out successfully!");
 }
+
+const dashlink =document.getElementById('dashboard');
+const dashSec =document.getElementById('dash-sec');
+
+const assetlink =document.getElementById('asset');
+const assetSec =document.getElementById('asset-sec');
+
+const userlink =document.getElementById('user');
+const userSec =document.getElementById('user-sec');
+
+const repolink =document.getElementById('report');
+const repoSec =document.getElementById('repo-sec');
+
+const settlink =document.getElementById('setting');
+const settSec =document.getElementById('sett-sec');
+
+assetlink.addEventListener('click',function(e){
+    e.preventDefault();
+    removeBackColor();
+    assetSec.style.display="block";
+    dashSec.style.display="none";
+    userSec.style.display="none";
+    repoSec.style.display="none";
+    settSec.style.display="none";
+    assetlink.style.backgroundColor=" rgb(215, 221, 243)";
+ })
+ dashlink.addEventListener('click',function(e){
+    e.preventDefault();
+    removeBackColor();
+    dashSec.style.display="block";
+    assetSec.style.display="none";
+    userSec.style.display="none";
+    repoSec.style.display="none";
+    settSec.style.display="none";
+    dashlink.style.backgroundColor=" rgb(215, 221, 243)";
+ })
+ userlink.addEventListener('click',function(e){
+    e.preventDefault();
+    removeBackColor();
+    userSec.style.display="block";
+    dashSec.style.display="none";
+    assetSec.style.display="none";
+    repoSec.style.display="none";
+    settSec.style.display="none";
+    userlink.style.backgroundColor=" rgb(215, 221, 243)";
+ })
+ repolink.addEventListener('click',function(e){
+    e.preventDefault();
+    removeBackColor();
+    repoSec.style.display="block";
+    dashSec.style.display="none";
+    assetSec.style.display="none";
+    userSec.style.display="none";
+    settSec.style.display="none";
+    repolink.style.backgroundColor=" rgb(215, 221, 243)";
+ })
+ settlink.addEventListener('click',function(e){
+    e.preventDefault();
+    removeBackColor();
+    settSec.style.display="block";
+    dashSec.style.display="none";
+    assetSec.style.display="none";
+    userSec.style.display="none";
+    repoSec.style.display="none";
+    settlink.style.backgroundColor=" rgb(215, 221, 243)";
+ })
+ function removeBackColor(){
+    dashlink.style.backgroundColor="";
+    assetlink.style.backgroundColor="";
+    repolink.style.backgroundColor="";
+    userlink.style.backgroundColor="";
+    settlink.style.backgroundColor="";
+ }

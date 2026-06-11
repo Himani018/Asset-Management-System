357da2a219ecb34248fbc2ab9aaae40f56701db2
Himani018 <himanibahuguna69@gmail.com>
2026-06-11 16:01:25 +0530

    creates docker file

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,11 @@
-<?php
-$dsn ="mysql:host=localhost;dbname=amsdb";
-$dbusername ="root";
-$dbpassword ="";
+<?php/
 
+$host = getenv('DB_HOST') ?: 'localhost';
+$dbname = getenv('DB_NAME') ?: 'inventory';
+$dbusername = getenv('DB_USER') ?: 'root';
+$dbpassword = getenv('DB_PASSWORD') ?: '';
+
+$dsn ="mysql:host=$host;dbname=$dbname";
 try{ //pdo php data object
      $pdo = new PDO($dsn,$dbusername ,$dbpassword);
      $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);

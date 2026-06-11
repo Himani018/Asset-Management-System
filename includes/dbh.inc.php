32434550ba85b3ce50055c80f31fb73b6b859c02
Himani018 <himanibahuguna69@gmail.com>
2026-06-11 16:32:14 +0530

    adds port

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,8 +4,10 @@ $host = getenv('DB_HOST') ?: 'localhost';
 $dbname = getenv('DB_NAME') ?: 'inventory';
 $dbusername = getenv('DB_USER') ?: 'root';
 $dbpassword = getenv('DB_PASSWORD') ?: '';
+$port = getenv('DB_PORT')?:3306;
 
-$dsn ="mysql:host=$host;dbname=$dbname";
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
 try{ //pdo php data object
      $pdo = new PDO($dsn,$dbusername ,$dbpassword);
      $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
